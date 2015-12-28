94c501635597701c448b53eba2ce12ebf4e65287
Ahilen <ahilenk@hotmail.com>
2015-12-28 18:27:04 -0500

    Age Calculator

---
+++ www/js/agecalculator.js
@@ -0,0 +1,98 @@
+/**
+ * Created by Ahilen on 2015-12-28.
+ */
+
+
+
+$(document).ready(function fn(){
+
+    $("#btncalc").click(function agecalculator(){
+
+        var name=$("#fullname").val();
+        var bday=$("#bday").val();
+
+       var age=getAge(bday);
+       $(".lblage").text("Your are ");
+        var lblage =$(".lblage").val();
+        $(".resname").text("Hi, "+name + lblage);
+        $(".resage").text(age);
+
+    })
+
+});
+
+
+function getAge(dateString) {
+    var now = new Date();
+    var today = new Date(now.getYear(),now.getMonth(),now.getDate());
+
+    var yearNow = now.getYear();
+    var monthNow = now.getMonth();
+    var dateNow = now.getDate();
+
+
+
+    var dob=  new  Date(dateString);
+    var yearDob = dob.getYear();
+    var monthDob = dob.getMonth();
+    var dateDob = dob.getDate();
+    var age = {};
+    var ageString = "";
+    var yearString = "";
+    var monthString = "";
+    var dayString = "";
+
+
+    yearAge = yearNow - yearDob;
+
+    if (monthNow >= monthDob)
+        var monthAge = monthNow - monthDob;
+    else {
+        yearAge--;
+        var monthAge = 12 + monthNow -monthDob;
+    }
+
+    if (dateNow >= dateDob)
+        var dateAge = dateNow - dateDob;
+    else {
+        monthAge--;
+        var dateAge = 31 + dateNow - dateDob;
+
+        if (monthAge < 0) {
+            monthAge = 11;
+            yearAge--;
+        }
+    }
+
+    age = {
+        years: yearAge,
+        months: monthAge,
+        days: dateAge
+    };
+
+    if ( age.years > 1 ) yearString = " years";
+    else yearString = " year";
+    if ( age.months> 1 ) monthString = " months";
+    else monthString = " month";
+    if ( age.days > 1 ) dayString = " days";
+    else dayString = " day";
+
+
+    if ( (age.years > 0) && (age.months > 0) && (age.days > 0) )
+        ageString = age.years + yearString + ", " + age.months + monthString + ", and " + age.days + dayString + " old.";
+    else if ( (age.years == 0) && (age.months == 0) && (age.days > 0) )
+        ageString = "Only " + age.days + dayString + " old!";
+    else if ( (age.years > 0) && (age.months == 0) && (age.days == 0) )
+        ageString = age.years + yearString + " old. Happy Birthday!!";
+    else if ( (age.years > 0) && (age.months > 0) && (age.days == 0) )
+        ageString = age.years + yearString + " and " + age.months + monthString + " old.";
+    else if ( (age.years == 0) && (age.months > 0) && (age.days > 0) )
+        ageString = age.months + monthString + " and " + age.days + dayString + " old.";
+    else if ( (age.years > 0) && (age.months == 0) && (age.days > 0) )
+        ageString = age.years + yearString + " and " + age.days + dayString + " old.";
+    else if ( (age.years == 0) && (age.months > 0) && (age.days == 0) )
+        ageString = age.months + monthString + " old.";
+    else ageString = "Oops! Could not calculate age!";
+
+    return ageString;
+}
